c3b4b6a4a21d32474ab39b2c3bd730da0f163c9f
Niveathika <niveathika@wso2.com>
2019-10-14 14:19:47 +0530

    Add siddhi http sink

--- src/main/java/io/siddhi/sample/mock/data/SalesStreamDataGenerator.java
+++ src/main/java/io/siddhi/sample/mock/data/SalesStreamDataGenerator.java
@@ -17,11 +17,17 @@
  */
 package io.siddhi.sample.mock.data;
 
+import io.siddhi.core.SiddhiAppRuntime;
+import io.siddhi.core.SiddhiManager;
+import io.siddhi.core.stream.input.InputHandler;
+import io.siddhi.extension.io.http.sink.HttpSink;
+import io.siddhi.extension.map.json.sinkmapper.JsonSinkMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * Generate data for http source attached to ,
@@ -29,18 +35,19 @@ import java.net.URL;
  */
 public class SalesStreamDataGenerator {
     private static final Logger LOGGER = LoggerFactory.getLogger(SalesStreamDataGenerator.class);
+    private static final String URL_TEMPLATE = "http://0.0.0.0:{{port}}/sales";
 
-    private String urlTemplate = "http://0.0.0.0:{{port}}/sales";
-    private URL sinkURL;
+    private String sinkURL;
+    private ArrayList<Object[]> generatedEvents;
     private boolean isConfigValid = true;
-    private boolean isDataGenerated = false;
 
     SalesStreamDataGenerator(String port) {
         try {
-            String url = urlTemplate.replace("{{port}}", port);
-            this.sinkURL = new URL(url);
+            String url = URL_TEMPLATE.replace("{{port}}", port);
+            this.sinkURL = new URL(url).toString();
+            generatedEvents = new ArrayList<>();
         } catch (MalformedURLException e) {
-            LOGGER.error("Invalid URL given as second parameter! Cannot start generator");
+            LOGGER.error("Invalid URL given as first parameter! Cannot start generator.");
             isConfigValid = false;
         }
     }
@@ -50,15 +57,50 @@ public class SalesStreamDataGenerator {
             return;
         }
 
-        this.isDataGenerated = true;
-        LOGGER.info("Generate Data  successfully! ");
+        generatedEvents.add(new Object[]{System.currentTimeMillis(), "clothes", "shirt", "seller1", 1});
+        generatedEvents.add(new Object[]{System.currentTimeMillis(), "clothes", "blouse", "seller1", 2});
+        LOGGER.info("Generate Data successfully! ");
     }
 
 
     public void sendData() {
-        if (!this.isDataGenerated) {
+        if (this.generatedEvents.size() == 0) {
             return;
         }
+
+        SiddhiManager siddhiManager = new SiddhiManager();
+
+        siddhiManager.setExtension("sink:http", HttpSink.class);
+        siddhiManager.setExtension("sinkMapper:json", JsonSinkMapper.class);
+
+        String streams =
+                "define stream InputStream" +
+                "(timestamp long, categoryName string, productName string, sellerName string, quantity int);" +
+                "@sink(type='http',publisher.url='" + this.sinkURL + "', " +
+                "@map(type='json')) " +
+                "define stream PublisherStream" +
+                "(timestamp long, categoryName string, productName string, sellerName string, quantity int);";
+
+        String queries =
+                "from InputStream " +
+                "insert into PublisherStream;";
+
+        SiddhiAppRuntime siddhiAppRuntime = siddhiManager.createSiddhiAppRuntime(streams + queries);
+        InputHandler inputStream = siddhiAppRuntime.getInputHandler("InputStream");
+
+
+        siddhiAppRuntime.start();
+        try {
+            for (Object[] generatedEvent : generatedEvents) {
+                inputStream.send(generatedEvent);
+            }
+            Thread.sleep(5000);
+        } catch (InterruptedException ignored) {
+            //ignored
+        }
+
+        siddhiAppRuntime.shutdown();
+        siddhiManager.shutdown();
 
         LOGGER.info("Send success! " + this.sinkURL);
     }
